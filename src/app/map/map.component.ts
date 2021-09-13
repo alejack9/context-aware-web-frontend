59643b50c52c6aa25b3a3cda53c7f1c6f69a6f52
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-13 10:38:52 +0200

    added leaflet map component

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, AfterViewInit } from '@angular/core';
+import { map, Map, tileLayer } from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements AfterViewInit {
+  private map: Map;
+
+  private initMap(): void {
+    this.map = map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3,
+    });
+
+    const tiles = tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
