09bc0d27f15c615b20f9357069e2d8330a09af9b
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-23 11:28:38 +0200

    forced https

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function requireHTTPS(req, res, next) {
 }
 const express = require("express");
 const app = express();
-// app.use(requireHTTPS);
+app.use(requireHTTPS);
 app.use(express.static("./dist/context-aware-web-frontend"));
 
 app.get("/*", function (req, res) {
