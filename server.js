2367a88f85f283b5518efeaf7a50fe2674ca2e72
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-23 11:23:01 +0200

    heroku pt2

---
+++ server.js
@@ -0,0 +1,16 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+const express = require("express");
+const app = express();
+// app.use(requireHTTPS);
+app.use(express.static("./dist/context-aware-web-frontend"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/context-aware-web-frontend/" });
+});
+app.listen(process.env.PORT || 4422);
